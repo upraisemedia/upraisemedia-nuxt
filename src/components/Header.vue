a8f14981914d2ab25649e603c3c1c19f0b3a25b3
upraisemedia <info@upraisemedia.nl>
2024-05-05 11:08:49 +0200

    Development

---
+++ src/components/Header.vue
@@ -0,0 +1,132 @@
+<script setup>
+const navigationVisible = ref(false);
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <div class="flex justify-between items-center">
+                <div class="logo">
+                    <NuxtLink to="/">
+                        <img src="~/assets/img/logo.svg" alt="Logo Upraise Media" />
+                    </NuxtLink>
+                </div>
+                <nav :class="navigationVisible && 'is-active'" class="navigation">
+                    <ul>
+                        <li><NuxtLink to="/">Home</NuxtLink></li>
+                        <li><NuxtLink to="/">Over ons</NuxtLink></li>
+                        <li><NuxtLink to="/">Ons werk</NuxtLink></li>
+                        <li><NuxtLink to="/">Diensten</NuxtLink></li>
+                        <li><NuxtLink to="/">Contact</NuxtLink></li>
+                    </ul>
+                </nav>
+                <div class="mailto">
+                    <span>
+                        <Icon name="heroicons:envelope" size="20"/>
+                        <strong>Mail:</strong>
+                    </span>
+                    <NuxtLink to="mailto:info@upraisemedia.nl">
+                        info@upraisemedia.nl
+                    </NuxtLink>
+                </div>
+                <div class="hamburger">
+                    <IconsHamburger/>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    height: 7rem;
+    background-color: var(--color-lightgrey);
+    display: flex;
+    align-items: center;
+    width: 100%;
+
+    @screen xl {
+        height: 10.8rem;
+    }
+
+    .logo {
+        img {
+            height: 3rem;
+        }
+    }
+
+    .navigation {
+        z-index: 10;
+        position: absolute;
+        top: 7rem;
+        left: 0;
+        right: 0;
+        background-color: var(--color-white);
+        width: 100vw;
+        height: calc(100vh - 7rem);
+        padding: 4rem 2rem 2rem 2rem;
+        display: none;
+
+        @screen lg {
+            display: block;
+            position: static;
+            top: inherit;
+            width: auto;
+            height: auto;
+            padding: 0;
+            background-color: transparent;
+        }
+
+        ul {
+            display: flex;
+            flex-direction: column;
+            gap: 4.8rem;
+            list-style: none;
+            text-align: center;
+
+            @screen lg {
+                flex-direction: row;
+                gap: 3.2rem;
+            }
+
+            li {
+                color: var(--color-primary);
+                font-weight: var(--font-weight-medium);
+                font-family: var(--font-heading);
+                font-size: 2.4rem;
+
+                @screen lg {
+                    font-size: 1.6rem;
+                }
+            }
+        }
+    }
+
+    .mailto {
+        display: none;
+        font-weight: var(--font-weight-light);
+        align-items: center;
+        gap: 0.4rem;
+
+        @screen lg {
+            display: flex;
+        }
+
+        span {
+            display: flex;
+            align-items: center;
+            gap: 0.4rem;
+        }
+
+        strong {
+            color: var(--color-primary);
+        }
+    }
+
+    .hamburger {
+        @screen lg {
+            display: none;
+        }
+    }
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,3 @@
 <template>
-    <h1>Creative digital agency</h1>
-    <p>
-        Nunc eros mi, rhoncus sed orci non, luctus fringilla ligula. Mauris massa lacus, iaculis elend placer
-        scelerisque risus. Maecenas sed tortor molestie, satis nibh sit amet dapibus felis. Vivamus sed neque iaculis.
-    </p>
+
 </template>
